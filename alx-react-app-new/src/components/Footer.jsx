23e4a0b99d324fa8c92f4e088768bbb1d61afaf7
AhmedAresha90 <Ahmedphysical90@gmail.com>
2026-01-27 05:15:57 +0800

    Footer.jsx

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -1,8 +1,14 @@
 export default function Footer() {
   return (
-    <footer style={{ backgroundColor: "#222", color: "white", padding: "10px", textAlign: "center" }}>
-      <p>© 2026 My React App</p>
+    <footer
+      style={{
+        marginTop: "40px",
+        padding: "10px",
+        textAlign: "center",
+        backgroundColor: "#f2f2f2",
+      }}
+    >
+      © 2026 My Company
     </footer>
   );
 }
-
