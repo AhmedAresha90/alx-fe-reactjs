c6f8caa4156b566918fb1621ba40d39d822f223a
AhmedAresha90 <Ahmedphysical90@gmail.com>
2026-01-27 04:04:23 +0800

    Add files via upload

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,23 @@
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import UserProfile from './components/UserProfile';
+
+
+function App() {
+  return (
+    <div>
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+    </div>
+  );
+}
+
+export default App;
+<<<<<<< HEAD
+
+=======
+>>>>>>> 426d54e1afe3323c6a79465875d56c6fef17a29b
