6aceae769a89992b2fad9b1002d7472472a46ef8
AhmedAresha90 <Ahmedphysical90@gmail.com>
2026-01-27 04:36:07 +0800

    App.jsx

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,21 +1,13 @@
-import Header from "./components/Header";
-import UserProfile from "./components/UserProfile";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import Counter from "./components/Counter";
 
-export default function App() {
+function App() {
   return (
     <div>
-      <Header />
-      <div style={{ padding: "0 20px" }}>
-        <UserProfile
-          name="John Doe"
-          age={25}
-          bio="Frontend developer who loves building UI."
-        />
-      </div>
-      <MainContent />
-      <Footer />
+      <Counter />
     </div>
   );
 }
+
+export default App;
+
+
