2e506cd3f72fb2ca786ebd1c2209eb326ad98276
AhmedAresha90 <Ahmedphysical90@gmail.com>
2026-02-02 05:58:51 +0800

    RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,19 +1,12 @@
 // RecipeList component
-  import { useRecipeStore } from './recipeStore';
-export default function RecipeList() {
+ import { Link } from "react-router-dom";
 
-  const RecipeList = () => {
-    const recipes = useRecipeStore(state => state.recipes);
+{recipes.map((recipe) => (
+  <li key={recipe.id}>
+    <Link to={`/recipe/${recipe.id}`}>
+      {recipe.title}
+    </Link>
+  </li>
+))}
 
-    return (
-      <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
   
