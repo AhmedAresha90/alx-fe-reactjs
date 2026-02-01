15d78bec140af315c01bd7bfc752503f628e3956
AhmedAresha90 <Ahmedphysical90@gmail.com>
2026-02-02 06:43:58 +0800

    App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,12 +1,20 @@
-import SearchBar from "./components/SearchBar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+import RecipeDetail from "./components/RecipeDetail";
+import SearchBar from "./components/SearchBar";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <SearchBar />
-      <RecipeList />
-    </>
+
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
