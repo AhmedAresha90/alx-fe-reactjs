22c678e2f51bd73f52abafd329f071916f1716ca
AhmedAresha90 <Ahmedphysical90@gmail.com>
2026-02-02 05:50:50 +0800

    RecipeStore.js

---
+++ recipe-sharing-app/src/store/RecipeStore.js
@@ -0,0 +1,24 @@
+import create from "zustand";
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+
+  addRecipe: (recipe) =>
+    set((state) => ({
+      recipes: [...state.recipes, recipe],
+    })),
+
+  updateRecipe: (id, updatedData) =>
+    set((state) => ({
+      recipes: state.recipes.map((r) =>
+        r.id === id ? { ...r, ...updatedData } : r
+      ),
+    })),
+
+  deleteRecipe: (id) =>
+    set((state) => ({
+      recipes: state.recipes.filter((r) => r.id !== id),
+    })),
+}));
+
+export default useRecipeStore;
